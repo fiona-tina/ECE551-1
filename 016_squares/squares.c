0d786d098b5ab672ff1c83f5e39a1446efd55eb6
rx29 <rx29@duke.edu>
2018-09-06 16:54:27 -0400

    016@5

--- 016_squares/squares.c
+++ 016_squares/squares.c
@@ -34,7 +34,8 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
           (((y >= y_offset) && (y < y_offset + size2)) &&
            (x == x_offset || x == x_offset + size2 - 1)))
         printf("*");
-      else if ((x < size1 && (y == 0 || y == size1 - 1)) || (y < size1 && (x == 0 || x == size1)))
+      else if ((x < size1 && (y == 0 || y == size1 - 1)) ||
+               (y < size1 && (x == 0 || x == size1 - 1)))
         printf("#");
       else
         printf(" ");
