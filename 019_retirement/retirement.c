ad065da469c3daeacaf7f9744f63a9ad786d6e3f
rx29 <rx29@duke.edu>
2018-09-10 11:29:27 -0400

    019@4

--- 019_retirement/retirement.c
+++ 019_retirement/retirement.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 typedef struct retire_info_tag {
-  int month;
+  int months;
   double contribution;
   double rate_of_return;
 } retire_info_t;
 
 void retirement(int startAge, double initial, retire_info_t working, retire_info_t retired) {
-  int totalMonth = working.month;
+  int totalMonth = working.months;
   int year = totalMonth / 12;
   int month = totalMonth % 12;
 
@@ -23,24 +23,24 @@ int main() {
   int startMonth = 327;
   double startIni = 21345;
   ;
-  working.month = startMonth;
+  working.months = startMonth;
   working.contribution = 1000;
   working.rate_of_return = 0.045 / 12;
 
   for (int i = 0; i < 489; i++) {
     retirement(startMonth, startIni, working, working);
-    working.month++;
+    working.months++;
     startIni += startIni * working.rate_of_return;
     startIni += working.contribution;
   }
 
-  retirementing.month = working.month;
+  retirementing.months = working.months;
   retirementing.contribution = -4000;
   retirementing.rate_of_return = 0.01 / 12;
 
   for (int i = 0; i < 384; i++) {
     retirement(startMonth, startIni, retirementing, working);
-    retirementing.month++;
+    retirementing.months++;
     startIni += startIni * retirementing.rate_of_return;
     startIni += retirementing.contribution;
   }
