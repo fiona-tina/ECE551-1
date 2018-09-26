e719c27392e2db3510024d7e92b74679f3789927
rx29 <rx29@duke.edu>
2018-09-26 16:34:08 -0400

    049@1

---
+++ 049_rot_matrix/rotateMatrix.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_line(char line[], FILE * f) {
+  int i;
+  int ch;
+
+  i = 0;
+  while (1) {
+    ch = fgetc(f);
+    if (ch == '\n') {
+      if (i == 10) {
+        line[10] = '\0';
+        return;
+        //finished a line
+      }
+      else {
+        fprintf(stderr, "%s\n", "line too short");
+        exit(EXIT_FAILURE);
+      }
+      break;
+    }
+    else if (ch == EOF) {
+      if (i == 10) {
+        fprintf(stderr, "%s\n", "unexpected EOF");
+        exit(EXIT_FAILURE);
+      }
+      else {
+        fprintf(stderr, "%s\n", "line too short");
+        exit(EXIT_FAILURE);
+      }
+      break;
+    }
+    else {
+      if (i < 10) {
+        line[i] = (char)ch;
+        i++;
+      }
+      else {
+        fprintf(stderr, "%s\n", "line too long");
+        exit(EXIT_FAILURE);
+      }
+      break;
+    }
+  }
+}
+
+void rot_matrix(char m[10][10]) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      printf(c %, m[9 - j][i]);
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char * argv[]) {
+  char m[10][10];
+  FILE * f;
+  int i, ch;
+
+  if (argc != 2) {  //too many arg
+    fprintf(stderr, "Usage: rotateMatrix input\n");
+    exit(EXIT_FAILURE);
+  }
+  if ((f = fopen(argv[1], "r")) == 0) {  //c
+    fprintf(stderr, "Cannot open %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  for (i = 0; i < 10; i++) {
+    read_line(m[i], f);
+  }
+  if ((ch = fgetc(f)) != EOF) {
+    fprintf(stderr, "Too long\n");
+    exit(EXIT_FAILURE);
+  }
+
+  fclose(f);
+
+  rot_matrix(m);
+
+  return 0;
+}
