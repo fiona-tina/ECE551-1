63313371d85dcca62e87396708aa482e509dd17e
rx29 <rx29@duke.edu>
2018-10-02 19:45:20 -0400

    057@1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,19 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
+  char * res;
+  size_t sz = 0;
+  sz = strlen(inputName);
+
+  res = malloc((sz + 8) * sizeof(res));
+
+  memcpy(res, inputName, sz);
+  memcpy(res + sz, ".counts", 8);
+  return res;
+
   //WRITE ME
 }
