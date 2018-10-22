aad1b3cc42bcdb70d5c7dc0b7e4c47cdde9cc5df
rx29 <rx29@duke.edu>
2018-10-22 13:51:54 -0400

    077@4

--- 077_sort_cpp/sortLines.cpp
+++ 077_sort_cpp/sortLines.cpp
@@ -6,31 +6,37 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
-  vector<string> strs;
-  string str;
-  if (argc > 2) {
-    cerr << "wrong inputsize" << endl;
-    exit(EXIT_FAILURE);
-  }
-  else if (argc == 2) {
-    ifstream is(argv[1]);
-    if (!is) {
-      cerr << "wrong inputsize" << endl;
-      exit(EXIT_FAILURE);
-    }
-    while (getline(is, str)) {
-      strs.push_back(str);
+  if (argc >= 2) {
+    for (int i = 1; i < argc; i++) {
+      ifstream is(argv[i]);
+      vector<string> strs;
+      string str;
+      if (!is) {
+        cerr << "wrong inputsize" << endl;
+        exit(EXIT_FAILURE);
+      }
+      while (getline(is, str)) {
+        strs.push_back(str);
+      }
+      sort(strs.begin(), strs.end());
+      vector<string>::iterator it;
+      for (it = strs.begin(); it != strs.end(); ++it) {
+        cout << *it << endl;
+      }
     }
   }
   else {
-    while (cin >> str) {
+    vector<string> strs;
+    string str;
+    while (getline(cin, str)) {
       strs.push_back(str);
     }
+    sort(strs.begin(), strs.end());
+    vector<string>::iterator it;
+    for (it = strs.begin(); it != strs.end(); ++it) {
+      cout << *it << endl;
+    }
   }
-  sort(strs.begin(), strs.end());
-  vector<string>::iterator it;
-  for (it = strs.begin(); it != strs.end(); ++it) {
-    cout << *it << endl;
-  }
+
   return 0;
 }
