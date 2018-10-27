f592e81f0cf915bbe65e74490c3fdc82d6096c29
rx29 <rx29@duke.edu>
2018-10-26 20:21:30 -0400

    082@1

---
+++ 082_expr_eval2/expr.h
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Expression
+{
+ public:
+  virtual string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression
+{
+ private:
+  long num;
+
+ public:
+  explicit NumExpression(long x) : num(x){};
+  virtual string toString() const {
+    stringstream ss;
+    ss << num;
+    return ss.str();
+  }
+  virtual ~NumExpression(){};
+};
+
+class OpExpression : public Expression
+{
+ private:
+  char ch;
+  string left;
+  string right;
+
+ public:
+  OpExpression(char ch, Expression * lhs, Expression * rhs) :
+      ch(ch),
+      left(lhs->toString()),
+      right(rhs->toString()){};
+  virtual string toString() const {
+    stringstream ss;
+    ss << "(" << left << " " << ch << " " << right << ")";
+    return ss.str();
+  }
+  ~OpExpression() {}
+};
+
+class PlusExpression : public OpExpression
+{
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : OpExpression('+', lhs, rhs){};
+};
+
+class MinusExpression : public OpExpression
+{
+ public:
+  MinusExpression(Expression * lhs, Expression * rhs) : OpExpression('-', lhs, rhs){};
+};
+
+class TimesExpression : public OpExpression
+{
+ public:
+  TimesExpression(Expression * lhs, Expression * rhs) : OpExpression('*', lhs, rhs){};
+};
+
+class DivExpression : public OpExpression
+{
+ public:
+  DivExpression(Expression * lhs, Expression * rhs) : OpExpression('/', lhs, rhs){};
+};
