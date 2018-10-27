40e639464d72c25067f990ca120830e70d95044c
rx29 <rx29@duke.edu>
2018-10-26 20:34:08 -0400

    083@1

---
+++ 083_expr_eval3/expr.h
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Expression
+{
+ public:
+  virtual long evaluate() const = 0;  //abstract method
+  virtual string toString() const = 0;
+  virtual ~Expression() {}
+};
+
+class NumExpression : public Expression
+{
+ private:
+  long num;
+
+ public:
+  virtual long evaluate() const { return num; }
+  explicit NumExpression(long x) : num(x){};
+  virtual string toString() const {
+    stringstream ss;
+    ss << num;
+    return ss.str();
+  }
+  virtual ~NumExpression(){};
+};
+
+class OpExpression : public Expression
+{
+ protected:
+  char ch;
+  string left;
+  string right;
+  long leftNum;
+  long rightNum;
+
+ public:
+  OpExpression(char ch, Expression * lhs, Expression * rhs) :
+      ch(ch),
+      left(lhs->toString()),
+      right(rhs->toString()) {
+    delete lhs;
+    delete rhs;
+  };
+  virtual string toString() const {
+    stringstream ss;
+    ss << "(" << left << " " << ch << " " << right << ")";
+    return ss.str();
+  }
+
+  ~OpExpression() {}
+};
+
+class PlusExpression : public OpExpression
+{
+ public:
+  PlusExpression(Expression * lhs, Expression * rhs) : OpExpression('+', lhs, rhs){};
+  virtual long evaluate() const { return leftNum + rightNum; }
+};
+
+class MinusExpression : public OpExpression
+{
+ public:
+  MinusExpression(Expression * lhs, Expression * rhs) : OpExpression('-', lhs, rhs){};
+  virtual long evaluate() const { return leftNum - rightNum; }
+};
+
+class TimesExpression : public OpExpression
+{
+ public:
+  TimesExpression(Expression * lhs, Expression * rhs) : OpExpression('*', lhs, rhs){};
+  virtual long evaluate() const { return leftNum * rightNum; }
+};
+
+class DivExpression : public OpExpression
+{
+ public:
+  DivExpression(Expression * lhs, Expression * rhs) : OpExpression('/', lhs, rhs){};
+  virtual long evaluate() const { return leftNum / rightNum; }
+};
