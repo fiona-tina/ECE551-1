c26dba5595b67085517beef84962c0a61d4b30ff
rx29 <rx29@duke.edu>
2018-11-11 16:15:21 -0500

    101@1

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,18 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t pq;
+  for (int i = 0; i < 257; ++i) {
+    pq.push(new Node(i, counts[i]));
+  }
+  while (pq.size() > 1) {
+    Node * right = pq.top();
+    pq.pop();
+    Node * left = pq.top();
+    pq.pop();
+    Node * newRoot = new Node(left, right);
+    pq.push(newRoot);
+  }
+  return pq.top();
+}
