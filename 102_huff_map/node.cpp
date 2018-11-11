90b7c42bbe80513c77d300fd3eb15f790d7f7af7
rx29 <rx29@duke.edu>
2018-11-11 16:46:26 -0500

    102@2

--- 102_huff_map/node.cpp
+++ 102_huff_map/node.cpp
@@ -1,5 +1,16 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
   //WRITE ME!
+  if (sym != NO_SYM) {
+    // a leaf
+    theMap[freq] = b;
+  }
+  else {
+    //not a leaf
+    assert(left != NULL && right != NULL);
+    BitString leftStr = b.plusZero();
+    BitString rightStr = b.plusOne();
+    left->buildMap(leftStr, theMap);
+    right->buildMap(rightStr, theMap);
+  }
 }
-
