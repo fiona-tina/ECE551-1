4f03fa6eac4d83fbde0b62d763462c626b9c667f
rx29 <rx29@duke.edu>
2018-11-12 12:59:41 -0500

    103@1

--- 103_huff_all/compress.cpp
+++ 103_huff_all/compress.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
-#include <stdio.h>
+#include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <functional>
+#include <iostream>
 #include <map>
 #include <queue>
-#include <assert.h>
-#include <functional>
-#include <stdlib.h>
-#include "readFreq.h"
+
 #include "node.h"
+#include "readFreq.h"
 
+using namespace std;
 
-
-void writeHeader(BitFileWriter * bfw, const std::map<unsigned,BitString> &theMap) {
-  for (int i =0 ; i < 257; i++) {
-    std::map<unsigned,BitString>::const_iterator it = theMap.find(i);
+void writeHeader(BitFileWriter * bfw, const std::map<unsigned, BitString> & theMap) {
+  for (int i = 0; i < 257; i++) {
+    std::map<unsigned, BitString>::const_iterator it = theMap.find(i);
     if (it != theMap.end()) {
       bfw->writeByte(it->second.size());
       bfw->writeBitString(it->second);
@@ -24,15 +26,27 @@ void writeHeader(BitFileWriter * bfw, const std::map<unsigned,BitString> &theMap
   }
 }
 
-void writeCompressedOutput(const char* inFile,
-			   const char *outFile,
-			   const std::map<unsigned,BitString> &theMap ){
+void writeCompressedOutput(const char * inFile,
+                           const char * outFile,
+                           const std::map<unsigned, BitString> & theMap) {
   BitFileWriter bfw(outFile);
-  writeHeader(&bfw,theMap);
+  writeHeader(&bfw, theMap);
 
   //WRITE YOUR CODE HERE!
   //open the input file for reading
+  FILE * ifile;
+  ifile = fopen(inFile, "r");
+  if (ifile == NULL) {
+    cout << "cant open";
+    exit(1);
+  }
 
+  char c;
+  while ((c = fgetc(ifile)) != EOF) {
+    bfw.writeBitString(theMap.find(c)->second);
+  }
+  bfw.writeBitString(theMap.find(256)->second);
+  fclose(ifile);
   //You need to read the input file, lookup the characters in the map,
   //and write the proper bit string with the BitFileWriter
 
@@ -44,14 +58,20 @@ void writeCompressedOutput(const char* inFile,
 
 int main(int argc, char ** argv) {
   if (argc != 3) {
-    fprintf(stderr,"Usage: compress input output\n");
+    fprintf(stderr, "Usage: compress input output\n");
     return EXIT_FAILURE;
   }
+  uint64_t * freq = readFrequencies(argv[1]);
+  Node * root = buildTree(freq);
+  map<unsigned, BitString> mp;
+  BitString b;
+  root->buildMap(b, mp);
+  writeCompressedOutput(argv[1], argv[2], mp);
   //WRITE YOUR CODE HERE
   //Implement main
-  //hint 1: most of the work is already done. 
+  //hint 1: most of the work is already done.
   //hint 2: you can look at the main from the previous tester for 90% of this
-
-
+  delete[] freq;
+  delete root;
   return EXIT_SUCCESS;
 }
